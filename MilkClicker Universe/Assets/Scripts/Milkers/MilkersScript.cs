a2c5353f106c36964b1774a9b08402ad2588bee7
PetarWho <petarpenev02@gmail.com>
2021-12-17 13:18:55 +0200

    Anchors

--- MilkClicker Universe/Assets/Scripts/Milkers/MilkersScript.cs
+++ MilkClicker Universe/Assets/Scripts/Milkers/MilkersScript.cs
@@ -7,49 +7,59 @@ public class MilkersScript : MonoBehaviour
 {
     public GameObject uiDisplayCost;
     public GameObject uiDisplayCost2;
-    
+
     private int timesUpgraded;
     public static double cost;
     public static double cost2;
-    private int number = 0;
-    
-        //milkers int///
-        
-        public static int milker1 = 0;
-        
-        /// //// ///
-        
-        
 
+    //milkers int///
+
+    public static int milker1 = 0;
+
+    /// //// ///
     public void Milkers(int milker)
     {
-        switch (milker)
+        if (cost != 0)
         {
-            case 1:
-                if (ClickTheMilk.liters >= cost)
-                {
-                    ClickTheMilk.liters -= cost;
-                    cost *= 1.5;
-                    MilkersMechanics.milkIncrease += 1;
-                    milker1 = 1;
-                }
-                break;
-            case 2 :
-                if (ClickTheMilk.liters >= cost2)
-                {
-                    ClickTheMilk.liters -= cost2;
-                    cost2 *= 1.5;
-                }
-                break;
+            switch (milker)
+            {
+                case 1:
+                    if (ClickTheMilk.liters >= cost)
+                    {
+                        ClickTheMilk.liters -= cost;
+                        cost *= 1.5;
+                        MilkersMechanics.milkIncrease += 1;
+                        milker1 = 1;
+                    }
+
+                    break;
+                case 2:
+                    if (ClickTheMilk.liters >= cost2)
+                    {
+                        ClickTheMilk.liters -= cost2;
+                        cost2 *= 1.5;
+                    }
+
+                    break;
+            }
         }
     }
-    
-    void Update()
+
+    private double MilkersPrice(double price)
     {
-        uiDisplayCost.GetComponent<Text>().text = Math.Round(cost).ToString();
-        uiDisplayCost2.GetComponent<Text>().text = Math.Round(cost2).ToString();
+        if (ClickTheMilk.liters >= price)
+        {
+            ClickTheMilk.liters -= price;
+            price *= 1.66;
+            return price;
+        }
+        return 0;
     }
 
-    
-}
 
+    void Update()
+        {
+            uiDisplayCost.GetComponent<Text>().text = Math.Round(cost).ToString();
+            uiDisplayCost2.GetComponent<Text>().text = Math.Round(cost2).ToString();
+        }
+    }
